6ee6b6885e19db247976160c28a579df0cb11201
Alpharivs <91545571+Alpharivs@users.noreply.github.com>
2024-01-31 20:20:55 +0100

    Update massScan.go

--- massScan/massScan.go
+++ massScan/massScan.go
@@ -16,7 +16,7 @@ import (
 func Scan(ip, inter, rate string) string {
 	// Docker fix
 	//masscanCmd := fmt.Sprintf("sudo docker run -i --network host --rm adarnimrod/masscan -p1-65535,U:1-65535 %s -e %s --rate=%s --wait=5", ip, inter, rate)
-	// exec.Command won't work because of sudo
+	// direct use of exec.Command won't work because of sudo
 	masscanCmd := fmt.Sprintf("sudo masscan %s -p1-65535,U:1-65535 -e %s --rate=%s --wait=5", ip, inter, rate)
 	cmd := exec.Command("bash", "-c", masscanCmd)
 	// Interruption Handler
